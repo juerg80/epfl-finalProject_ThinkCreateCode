35fc64ee5445fa91d58305f8fb057109899a88b4
juerg80epfl <juerg_mueller@vtxmail.ch>
2020-04-21 21:18:05 +0200

    WIP

--- messenger_classes.py
+++ messenger_classes.py
@@ -4,6 +4,7 @@ import datetime as dt
 
 ## Definition of classes and functions used in the game
 
+# Modul Init
 def init_config(file):
     config={}
     fobj=open(file,"r")
@@ -11,10 +12,25 @@ def init_config(file):
         line=line.strip()
         value=line.split(":")
         config[value[0]]=value[1]
-    # convert map string to list
+
+    # convert some strings to lists
     config["map1"]=config["map1"].split(sep=',')
+    config["RiderNames"]=config["RiderNames"].split(sep=',')
+
     fobj.close()
     return config
+
+def gen_rider(id,config):
+    name=config['f'+str(id) + '_name']
+    nickname=config['f'+str(id)+'_nickname']
+    avgSpeed=config['f'+str(id)+'_avgSpeed']
+    varSalary=config['f'+str(id)+'_varSalary']
+    fixSalary=config['f'+str(id)+'_fixSalary']
+    workload=config['f'+str(id)+'_workload']
+    reliability=config['f'+str(id)+'_reliability']
+
+    res=rider(name,nickname,avgSpeed,varSalary,fixSalary,reliability,workload)
+    return res
 
 def read_map(file):
     map = numpy.loadtxt(file)
@@ -24,17 +40,14 @@ def getStatus():
     pass
 
 # Modul: Prepare Next Shift
-def prepare_next_shift(shift_id,config,last_shift={}):
+def prepare_next_shift(shift_id,config,riders,last_shift={}):
     if shift_id==0: # initial state
         cash_last_shift=0
-        rider_list=[]
-        for i in range(0,int(config['NumberRiders'])):
-            rider_list.append('f'+str(i+1))
-        available_riders=rider_list
+        available_riders=riders
         time_shift={'t_start': dt.time(8,0,0),'t_end': dt.time(12,0,0)}
     else:
         cash_last_shift=get_cash_last_shift()
-        available_riders=get_available_riders()
+        available_riders=get_available_riders(riders)
         time_shift=get_time_shift()
     
     order_list=get_orders()
@@ -44,7 +57,8 @@ def prepare_next_shift(shift_id,config,last_shift={}):
     return new_shift
 
 def get_time_shift():
-    pass
+    #@work
+    return {'t_start': dt.time(8,0,0),'t_end': dt.time(12,0,0)}
 
 def get_cash_last_shift():
     #@work
@@ -53,15 +67,15 @@ def get_cash_last_shift():
 def get_orders():
     #@work
     order_list=[]
-    order_1=order('A','10:00','B','12:00')
-    order_2=order('C','09:00','D','11:30')
+    order_1=order('A',dt.time(10,0,0),'B',dt.time(12,0,0))
+    order_2=order('C',dt.time(9,0,0),'D',dt.time(11,30,0))
     order_list.append(order_1)
     order_list.append(order_2)
     return order_list
 
-def get_available_riders():
+def get_available_riders(riders):
     #@work
-    rider_list=['f1']
+    rider_list=riders
     return rider_list
 
 # Modul Build Shift
@@ -70,10 +84,15 @@ def transform_assignment(assignment_raw,current_shift):
     # transforms html params in dict
     num_riders=len(current_shift.availRiders)
     assignment={}
+    order_list=current_shift.orders
     for i in range (0,num_riders):
         assignment_list=assignment_raw[i].split(';')
         rider=current_shift.availRiders[i]
-        assignment.update({rider:assignment_list})
+        my_orderlist=[]
+        for i in assignment_list:
+            my_orderlist.append(order_list[int(i)-1])
+
+        assignment.update({rider:my_orderlist})
     return assignment
 
 
@@ -81,11 +100,11 @@ def check_assignment(assignment):
     #@work
     return True
 
-def build_shift(current_shift,assignment):
+def build_shift(current_shift,assignment,map):
     tours={}
     num_riders=len(assignment)
     for i in range(0,num_riders):
-        tour=get_tour(assignment[current_shift.availRiders[i]])
+        tour=get_tour(current_shift.availRiders[i],assignment[current_shift.availRiders[i]],map)
         tours.update({current_shift.availRiders[i]:tour})
 
     current_shift.tours=tours
@@ -93,12 +112,28 @@ def build_shift(current_shift,assignment):
     return current_shift
 
 
-def get_tour(assignment_i):
-    #@work
-    
+def get_tour(my_rider,my_orders,map):
+    num_orders=len(my_orders)
+    num_steps=2+num_orders+(num_orders-1)
+    my_tour=[]
+    count_order=-1
+    for i in range(0,num_steps):
+        if i==0:
+            flag='Transfer_init'
+        elif i==num_steps-1:
+            flag='Transfer_end'
+        elif i%2==0:
+            flag='Transfer'
+        else:
+            flag='Order'
+            count_order=count_order+1
 
+        my_step=step(flag,my_orders[count_order].start_time,my_orders[count_order].end_time,
+                    my_orders[count_order].start_loc,my_orders[count_order].end_loc,my_orders[count_order].volume,
+                    my_rider,map)
+        my_tour.append(my_step)
+    return my_tour
 
-  
 
 # Classes
 class order:
@@ -123,6 +158,7 @@ class rider:
         self.varSalary=varSalary
         self.fixSalary=fixSalary
         self.varSalary=varSalary
+        self.reliability=reliability
         self.workload=workload # pensum
 
 class shift:
@@ -146,10 +182,57 @@ class shift:
         return 75
 
 class tour:
-    pass
+    def __init__(self,rider,steps):
+        self.rider=rider
+        self.steps=steps
 
 class step:
-    pass
+    def __init__(self,flag,t_start,t_end_soll,loc_start,loc_end,volume,rider,map):
+        self.flag=flag
+        self.t_start=t_start
+        self.t_end_soll=t_end_soll
+        self.loc_start=loc_start
+        self.loc_end=loc_end
+        self.volume=volume
+        self.rider=rider
+        self.map=map
+        self.avgSpeed_ist=self.get_avgSpeed_ist(rider)
+        self.t_end_ist=self.get_t_end_ist()
+        self.bike_issue=self.get_bikeIssue()
+        
+
+    def get_bikeIssue(self):
+        #@work
+        # 0: no issue
+        # 1: minor_issue
+        # 2: big_issue
+        # 3: major_issue
+        return 0
+
+    def get_avgSpeed_ist(self,rider):
+        #@work
+        avgSpeed=int(rider.avgSpeed)
+        return avgSpeed
+
+    def get_t_end_ist(self):
+        my_distance=self.map.get_distance(self.loc_start,self.loc_end)
+        t_end_ist=dt.time(self.t_start.hour, self.t_start.minute + int((my_distance/self.avgSpeed_ist)*60))
+        return t_end_ist
+
+
+class map:
+    def __init__(self,locations,distances,altitudes):
+        self.locations=locations
+        self.altitudes=altitudes
+        self.distances=distances
+    
+    def get_distance(self,loc_start,loc_end):
+        #@work
+        return 20
+
+    def get_altitudes(self,loc_start,loc_end):
+        #@work
+        return 50
 
 class company:
     pass
--- messenger_simul_main.py
+++ messenger_simul_main.py
@@ -12,9 +12,10 @@ shift_id=0
 config={}
 map_distances=[]
 map_altitudes=[]
-map={}
+my_map=map([],[],[])
 current_shift={}
 last_shift={}
+riders=[]
 
 
 # define some functions
@@ -36,15 +37,22 @@ def init():
     global shift_id
     global current_shift
     global last_shift
+    global my_map
+    global riders
 
     config=init_config(config_File)
     shift_id=0
     map_distances=read_map(map_distances_file)
     map_altitudes=read_map(map_altitudes_file)
-    map={"distances":map_distances,"altitudes":map_altitudes}
-    current_shift={}
+    my_map=map(config['map1'],map_distances,map_altitudes)
+    riders=[]
+    for i in range (0,int(config['NumberRiders'])):
+        res=gen_rider(i+1,config)
+        riders.append(res)
+
+    current_shift=shift(dt.time(8,0,0),dt.time(12,0,0),shift_id,[],[],[])
     last_shift={}
-    return get_html("index").replace("$$STATUS$$","Game initialised")
+    return get_html("index").replace("$$STATUS$$","Game initialised").replace("$$VARSTATS$$","Shift ID: " + str(shift_id))
 
 @app.route("/PrepareNextShift")
 def PrepareNextShift():
@@ -52,37 +60,40 @@ def PrepareNextShift():
     global last_shift
 
     last_shift=current_shift
-    current_shift=prepare_next_shift(shift_id,config,last_shift)
+    current_shift=prepare_next_shift(current_shift.shift_id,config,riders,last_shift)
     
     # Build html lists
     content_riders=current_shift.availRiders
     result_riders=""
     for line in content_riders:
-         result_riders+="<p class='availRider'>"+ line +"</p>"
+         result_riders+="<p class='availRider'>"+ line.name +"</p>"
 
     content_orders=current_shift.orders
     result_orders=""
     count=1
     for line in content_orders:
-        result_orders+="<p class='availOrders'>"+ 'O' +str(count) +': ' + line.start_loc + line.start_time + line.end_loc + line.end_time + str(line.volume) +"</p>"
+        result_orders+="<p class='availOrders'>"+ str(count) +': ' + line.start_loc + ' - ' + line.start_time.strftime('%H:%M')  + ' - ' + line.end_loc + ' - '+ line.end_time.strftime('%H:%M') + ' - '+ str(line.volume) +"</p>"
         count=count+1
 
     # Build Forms
     num_riders=len(current_shift.availRiders)
     forms=""
     for i in range(0,num_riders):
-        forms+=current_shift.availRiders[i] + ": " + "<input type='text' name=" + current_shift.availRiders[i] + " class='assign_rider_spec' value='Enter Orders in chronological order, separeted with ;'>"
+        forms+=current_shift.availRiders[i].name + ": " + "<input type='text' name=" + current_shift.availRiders[i].name + " class='assign_rider_spec' value='Enter Orders in chronological order, separeted with ;'>"
     
     return get_html("assignment").replace("$$AVAILABLERIDERS$$",result_riders).replace("$$AVAILORDERS$$",result_orders).replace("$$ASSIGNMENT$$",forms)
     
 
 @app.route("/GetRiderAssignment")
 def GetRiderAssignment():
+    global current_shift
+    global last_shift
+
     num_riders=len(current_shift.availRiders)
-    assignment_raw=[]
+    assignment_raw=[] 
     for i in range(0,num_riders):
-        assignment=flask.request.args.get(current_shift.availRiders[i])
-        assignment_raw.append(assignment)
+        assignment=flask.request.args.get(current_shift.availRiders[i].name)
+        assignment_raw.append(assignment) #=['2,1']
     
     # transform and check assignment
     assignment=transform_assignment(assignment_raw,current_shift)
@@ -93,10 +104,7 @@ def GetRiderAssignment():
         pass
 
     # Build new shift
-    build_shift()
+    current_shift=build_shift(current_shift,assignment,my_map)
 
-    return get_html("index").replace("$$STATUS$$","Riders assigned")
-
-@app.route("/BuildShift")
-def BuildShift():
-    pass
+    return get_html("index").replace("$$STATUS$$","Riders assigned").replace("$$VARSTATS$$",'Shift ID: '+ str(current_shift.shift_id))
+    
